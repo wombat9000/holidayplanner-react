c17e48322471d1cce59b790969ab3b80457cf0a0
bstein <bstein@thoughtworks.com>
2018-08-09 20:09:04 +0200

    added first test, enzyme, made linter less nasty

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,16 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './App';
+import {shallow, ShallowWrapper} from 'enzyme';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+it('should have Holiday as default title', () => {
+    const shallowWrapper: ShallowWrapper = shallow(<App/>);
+
+    expect(shallowWrapper.state('title')).toBe('Holiday');
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,38 @@
+import {AppBar, Toolbar, Typography} from '@material-ui/core';
+import CssBaseline from '@material-ui/core/CssBaseline';
 import * as React from 'react';
 import './App.css';
-
 import logo from './logo.svg';
 
-class App extends React.Component {
-  public render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+
+class App extends React.Component<any, AppState> {
+    constructor(props: any) {
+        super(props);
+        this.state = {title: 'Holiday'};
+    }
+
+    public render() {
+        return (
+            <div className="App">
+                <CssBaseline/>
+                <AppBar position="static">
+                    <Toolbar variant="dense">
+                        <Typography variant="title" color="inherit">
+                            {this.state.title}
+                        </Typography>
+                    </Toolbar>
+                </AppBar>
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <h1 className="App-title">Welcome to React</h1>
+                </header>
+            </div>
+        );
+    }
+}
+
+interface AppState {
+    title: string
 }
 
 export default App;
